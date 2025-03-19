321301afd4e268c98c165544faf7e710c573af99
selimgunaydin <sselimgunaydin@gmail.com>
2025-03-19 03:22:17 +0300

    Refactor RichTextEditor to enhance text styling capabilities
    - Integrated color selection and text styling features using @tiptap/extension-color and @tiptap/extension-text-style.
    - Improved user experience by updating editor attributes and styling.

---
+++ seeds/seed-admin.ts
@@ -0,0 +1,29 @@
+import { db } from "../shared/db";
+import { admin_users } from "../shared/schemas";
+import { scrypt, randomBytes, timingSafeEqual } from "crypto";
+import { promisify } from "util";
+
+const scryptAsync = promisify(scrypt);
+
+export async function hashPassword(password: string) {
+    const salt = randomBytes(32).toString("hex");
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+    return `${buf.toString("hex")}.${salt}`;
+  }
+
+async function seedAdmin() {
+  try {
+    const password = await hashPassword("user");
+    
+    await db.insert(admin_users).values({
+      username: "password",
+      password,
+    });
+
+    console.log("Admin user created successfully!");
+  } catch (error) {
+    console.error("Error creating admin user:", error);
+  }
+}
+
+seedAdmin(); 
