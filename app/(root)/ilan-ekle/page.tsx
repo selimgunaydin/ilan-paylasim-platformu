4b85c791a61d611a2e32120ac08ff59146340d14
selimgunaydin <sselimgunaydin@gmail.com>
2025-03-22 04:24:42 +0300

    Implement user status check in CreateListingPage to manage free ad limits
    - Added fetchUserStatus function to retrieve user status from the API.
    - Displayed a message when the user has reached their free ad limit.
    - Updated CreateListingPage to handle user status before rendering the listing view.

--- app/(root)/ilan-ekle/page.tsx
+++ app/(root)/ilan-ekle/page.tsx
@@ -1,5 +1,5 @@
-import CreateListingView from '@/views/root/ilan-ekle'
-
+import CreateListingView from "@/views/root/ilan-ekle";
+import { headers } from "next/headers";
 export async function generateMetadata() {
   return {
     title: "İlan Ekle",
@@ -7,8 +7,35 @@ export async function generateMetadata() {
   };
 }
 
-export default function CreateListingPage() {
-  return (
-    <CreateListingView />
-  )
+const fetchUserStatus = async () => {
+  const headersList = headers();
+  const cookies = headersList.get("cookie");
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/user/status`,
+    {
+      headers: {
+        Cookie: cookies || "",
+      },
+      cache: "no-store",
+    }
+  );
+  if (!response.ok) throw new Error(response.statusText);
+  return response.json();
+};
+
+export default async function CreateListingPage() {
+  const userStatus = await fetchUserStatus();
+
+  if (
+    userStatus.user.used_free_ad === 1 &&
+    userStatus.user.yuksekUye === false
+  ) {
+    return (
+      <div className="flex flex-col gap-2 items-center justify-center h-screen">
+        <h1>Ücretsiz ilan limitiniz doldu.</h1>
+      </div>
+    );
+  }
+
+  return <CreateListingView />;
 }
